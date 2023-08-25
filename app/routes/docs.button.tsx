61256836db97633790ea41620344f7e5741778bd
Jory Phillips <jorylphillips@gmail.com>
2023-08-25 12:13:00 -0700

    chore: initial button docs

---
+++ app/routes/docs.button.tsx
@@ -0,0 +1,19 @@
+import { Link } from "@remix-run/react";
+import { VStack, Heading, Button } from "~/components/ui";
+
+export default function AlertDialogRoute() {
+  return (
+    <VStack gap={8} width="100%" alignItems="flex-start">
+      <Heading>Button</Heading>
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="subtle">Subtle</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button asChild variant="link">
+        <Link to="/docs/button">Link</Link>
+      </Button>
+      <Button isDisabled>Disabled</Button>
+    </VStack>
+  );
+}

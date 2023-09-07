02071e87fa651158749a9840ef5ef7504f3a2958
Jory Phillips <jorylphillips@gmail.com>
2023-09-07 12:20:11 -0700

    chore: add dropdown doc page

---
+++ app/routes/docs.dropdown.tsx
@@ -0,0 +1,27 @@
+import {
+  VStack,
+  Heading,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  Button,
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "~/components/ui";
+
+export default function DropdownRoute() {
+  return (
+    <VStack gap={8} width="100%" alignItems="flex-start">
+      <Heading>Dropdown</Heading>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button>Open</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Item 1</DropdownMenuItem>
+          <DropdownMenuItem>Item 2</DropdownMenuItem>
+          <DropdownMenuItem>Item 3</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </VStack>
+  );
+}

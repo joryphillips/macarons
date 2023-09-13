4c66c9b57e3c1061021109febad97a69a83b6d33
Jory Phillips <jorylphillips@gmail.com>
2023-09-13 14:35:00 -0700

    feat: Input size options, error state, css vars

--- app/components/ui/Input/Input.css.ts
+++ app/components/ui/Input/Input.css.ts
@@ -1,21 +1,21 @@
-import { style } from "@vanilla-extract/css";
+import { style, styleVariants } from "@vanilla-extract/css";
 import { vars } from "~/theme/theme.css";
+import { uIText } from "../Text/Text.css";
 
 const { colors } = vars;
-const { standard } = colors;
+const { standard, destructive } = colors;
 
 export const inputStyles = style({
   display: "flex",
   alignItems: "center",
-  height: "2.5rem",
+  height: vars.spacing[10],
   width: "100%",
   borderRadius: vars.border.radius.sm,
-  borderWidth: "1px",
+  borderWidth: vars.border.width.standard,
   borderStyle: "solid",
   borderColor: standard.elementBorder,
   backgroundColor: "transparent",
-  padding: "0.5rem 0.75rem",
-  fontSize: "1rem",
+  padding: `${vars.spacing[2]} ${vars.spacing[3]}`,
   color: standard.highContrastText,
   transitionProperty: vars.transitionPropertyCommon,
   transitionDuration: "200ms",
@@ -37,6 +37,40 @@ export const inputStyles = style({
   },
 });
 
+export const inputSizeVariants = styleVariants({
+  default: [
+    {
+      height: vars.spacing[10],
+      padding: `${vars.spacing[2]} ${vars.spacing[4]}`,
+    },
+    uIText["default"],
+  ],
+  sm: [
+    {
+      height: vars.spacing[8],
+      padding: `${vars.spacing[2]} ${vars.spacing[3]}`,
+    },
+    uIText["sm"],
+  ],
+});
+
+export const invalid = style({
+  borderColor: destructive.elementBorder,
+  selectors: {
+    "&:focus": {
+      outline: "none",
+      borderColor: "transparent",
+    },
+    "&:focus, &:focus-visible, &:focus-within": {
+      boxShadow: `0 0 0 2px ${destructive.elementBorder}`,
+    },
+  },
+});
+
 export const textAreaStyles = style({
   height: "5rem",
+});
+
+export const errorTextStyles = style({
+  color: destructive.lowContrastText,
 });
--- app/components/ui/Input/Input.tsx
+++ app/components/ui/Input/Input.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { X } from "lucide-react";
+import type { InputHTMLAttributes } from "react";
 import { forwardRef, useRef } from "react";
 import { base as baseReset, input as inputReset } from "~/styles/reset.css";
 import { Button } from "../Button/Button";
@@ -8,23 +9,48 @@ import { Box } from "../Layout/Box";
 import { VStack } from "../Layout/Stack";
 import { Text } from "../Text/Text";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../Tooltip/Tooltip";
-import { inputStyles } from "./Input.css";
+import {
+  errorTextStyles,
+  inputSizeVariants,
+  inputStyles,
+  invalid,
+} from "./Input.css";
 
 export const defaultScrollBehavior: ScrollIntoViewOptions = {
   behavior: "smooth",
   block: "nearest",
 };
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+type InputAttributes = Omit<InputHTMLAttributes<HTMLInputElement>, "size">;
+
+export interface InputProps extends InputAttributes {
+  size?: keyof typeof inputSizeVariants;
   isDisabled?: boolean;
-};
+  isInvalid?: boolean;
+}
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ isDisabled = false, className, ...props }, ref) => {
+  (
+    {
+      size = "default",
+      isDisabled = false,
+      isInvalid = false,
+      className,
+      ...props
+    },
+    ref
+  ) => {
     return (
       <input
         disabled={isDisabled}
-        className={clsx(baseReset, inputReset, inputStyles, className)}
+        className={clsx(
+          baseReset,
+          inputReset,
+          inputSizeVariants[size],
+          inputStyles,
+          isInvalid && invalid,
+          className
+        )}
         ref={ref}
         {...props}
       />
@@ -36,46 +62,64 @@ Input.displayName = "Input";
 const InputWithLabel = forwardRef<
   HTMLInputElement,
   InputProps & { label: string; onClear?: () => void }
->(({ label, isDisabled = false, className, name, onClear, ...props }, ref) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const labelId = `${label.toLowerCase().replace(" ", "_")}-label`;
-  return (
-    <VStack width="100%" alignItems="flex-start" spacing={2}>
-      <Label htmlFor={labelId}>{label}</Label>
-      <Box width="100%" position="relative">
-        <Input
-          ref={inputRef}
-          name={name}
-          id={labelId}
-          isDisabled={isDisabled}
-          className={className}
-          {...props}
-        />
-        {onClear && !!props.value && (
-          <Box position="absolute" right={0} top={0} padding={1}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  aria-label="Clear selection"
-                  variant="ghost"
-                  size="sm"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    onClear();
-                    inputRef.current?.focus();
-                  }}
-                >
-                  <X />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>Clear selection</TooltipContent>
-            </Tooltip>
-          </Box>
-        )}
-      </Box>
-    </VStack>
-  );
-});
+>(
+  (
+    {
+      label,
+      isDisabled = false,
+      isInvalid = false,
+      size = "default",
+      className,
+      name,
+      onClear,
+      ...props
+    },
+    ref
+  ) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const labelId = `${label.toLowerCase().replace(" ", "_")}-label`;
+    return (
+      <VStack width="100%" alignItems="flex-start" spacing={1}>
+        <Label fontWeight="medium" color="lowContrastText" htmlFor={labelId}>
+          {label}
+        </Label>
+        <Box width="100%" position="relative">
+          <Input
+            ref={inputRef}
+            name={name}
+            id={labelId}
+            isDisabled={isDisabled}
+            isInvalid={isInvalid}
+            size={size}
+            className={className}
+            {...props}
+          />
+          {onClear && !!props.value && (
+            <Box position="absolute" right={0} top={0} padding={1}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    aria-label="Clear selection"
+                    variant="ghost"
+                    size="sm"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      onClear();
+                      inputRef.current?.focus();
+                    }}
+                  >
+                    <X />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>Clear selection</TooltipContent>
+              </Tooltip>
+            </Box>
+          )}
+        </Box>
+      </VStack>
+    );
+  }
+);
 InputWithLabel.displayName = "InputWithLabel";
 
 const InputWithInfo = forwardRef<
@@ -96,25 +140,32 @@ const InputWithInfo = forwardRef<
       infoText,
       errorText,
       isDisabled = false,
+      size = "default",
       className,
       ...props
     },
     ref
   ) => {
     return (
-      <VStack width="100%" alignItems="flex-start" spacing={2}>
+      <VStack width="100%" alignItems="flex-start" spacing={1}>
         <InputWithLabel
           ref={ref}
           name={name}
           label={label}
           isDisabled={isDisabled}
+          isInvalid={!!errorText}
+          size={size}
           className={className}
           autoComplete="off"
           {...props}
         />
         <VStack spacing={2} alignItems="flex-start" width="100%">
-          {!errorText && infoText && <Text size="sm">{infoText}</Text>}
-          {errorText && <Text size="sm">{errorText}</Text>}
+          {!errorText && infoText && <Text size="xs">{infoText}</Text>}
+          {errorText && (
+            <Text size="xs" className={errorTextStyles}>
+              {errorText}
+            </Text>
+          )}
         </VStack>
       </VStack>
     );
@@ -122,4 +173,4 @@ const InputWithInfo = forwardRef<
 );
 InputWithInfo.displayName = "InputWithInfo";
 
-export { Input, InputWithLabel, InputWithInfo };
+export { Input, InputWithInfo, InputWithLabel };

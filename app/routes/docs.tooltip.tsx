7884fbff130681074a79bd2766f8681b8c43a1d7
Jory Phillips <jory.l.phillips@gmail.com>
2024-08-12 11:34:39 -0700

    feat: add tooltip docs page

---
+++ app/routes/docs.tooltip.tsx
@@ -0,0 +1,31 @@
+import {
+  VStack,
+  Heading,
+  Text,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  Button,
+} from "~/components/ui";
+
+export default function TooltipRoute() {
+  return (
+    <VStack gap={8} width="100%" alignItems="flex-start">
+      <Heading>Tooltip</Heading>
+      <Text>
+        The tooltip component is used to display additional information when a
+        user hovers over a trigger element. The base Radix tooltip content
+        component will position itself dynamically based on how close window
+        edges it is.
+      </Text>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="outline">Hover over me</Button>
+        </TooltipTrigger>
+        <TooltipContent asChild>
+          <Text>This is the tooltip content</Text>
+        </TooltipContent>
+      </Tooltip>
+    </VStack>
+  );
+}

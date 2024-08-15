285812548f76a6595826c36e7bf5db95ff29d801
Jory Phillips <jory.l.phillips@gmail.com>
2024-08-14 19:15:35 -0700

    feat: add shadows doc page

---
+++ app/routes/docs.shadows.tsx
@@ -0,0 +1,45 @@
+import { Box, Text, VStack, Heading, HStack } from "~/components/ui";
+import { Sprinkles } from "~/styles/sprinkles.css";
+
+function ShadowBox({ shadow }: { shadow: Sprinkles["boxShadow"] }) {
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      padding={4}
+      borderRadius="md"
+      boxShadow={shadow}
+      backgroundColor="elementBackground"
+      color="highContrastText"
+      style={{
+        width: "90px",
+        height: "90px",
+      }}
+    >
+      {`${shadow}`}
+    </Box>
+  );
+}
+
+export default function ShadowsRoute() {
+  return (
+    <VStack gap={4} width="100%" alignItems="flex-start">
+      <Heading size="lg">Shadows</Heading>
+      <Text as="p">
+        Box and derived components (Stack, etc) have a boxShadow prop that
+        accepts a shadow key from the theme. This scale is very similar to
+        Tailwind CSS's shadow scale.
+      </Text>
+      <HStack gap={8}>
+        <ShadowBox shadow="sm" />
+        <ShadowBox shadow="base" />
+        <ShadowBox shadow="md" />
+        <ShadowBox shadow="lg" />
+        <ShadowBox shadow="xl" />
+        <ShadowBox shadow="2xl" />
+      </HStack>
+      <ShadowBox shadow="inner" />
+    </VStack>
+  );
+}

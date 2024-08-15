cd19d287e006512b266bbc133691f3063fe9d51a
Jory Phillips <jory.l.phillips@gmail.com>
2024-08-14 19:13:50 -0700

    chore: update doc page

--- app/routes/docs.tsx
+++ app/routes/docs.tsx
@@ -1,8 +1,20 @@
-import { NavLink, Outlet, useRouteLoaderData } from "@remix-run/react";
+import { Outlet, useRouteLoaderData } from "@remix-run/react";
 import type { UserTheme } from "~/components/ThemeProvider/ThemeProvider";
 import { ThemeProvider } from "~/components/ThemeProvider/ThemeProvider";
-import { Divider, HStack, Heading, VStack } from "~/components/ui";
+import { Box, Divider, HStack, Heading, VStack, Text } from "~/components/ui";
 import { TooltipProvider } from "~/components/ui/Tooltip/Tooltip";
+import { NavBarLink } from "~/components/ui/NavBarLink/NavBarLink";
+import { MacaronStack } from "~/components/logo/MacaronStack";
+
+function NavBarHeading({ children }: { children: React.ReactNode }) {
+  return (
+    <Box width="100%" padding={2}>
+      <Heading as="h3" size="sm" color="highContrastText">
+        {children}
+      </Heading>
+    </Box>
+  );
+}
 
 export default function UI() {
   const rootMatchesData = useRouteLoaderData("root");
@@ -17,66 +29,101 @@ export default function UI() {
           maxWidth="xl"
           marginX="auto"
           spacing={8}
-          padding={4}
+          paddingTop={4}
+          paddingX={4}
+          paddingBottom={8}
           alignItems="center"
         >
-          <VStack width="100%" alignItems="flex-start" spacing={5}>
-            <VStack width="100%" alignItems="flex-start" spacing={4}>
-              <Heading as="h1" size="2xl">
-                Macarons
+          {/* Page header */}
+          <VStack width="100%" gap={4} alignItems="center">
+            <Box marginY={2} width="100%">
+              <Heading
+                color="primaryHighContrastText"
+                align="center"
+                size="4xl"
+                style={{
+                  fontFamily: "Georgia, serif",
+                  fontWeight: "bold",
+                  fontStyle: "italic",
+                  transform: "scaleX(0.86)",
+                }}
+              >
+                macarons
               </Heading>
-              <Heading as="h2" size="md" color="lowContrastText">
-                Design System and Styled UI Components Built with Vanilla
-                Extract and Radix
-              </Heading>
-            </VStack>
+              <HStack gap={0} justifyContent="center" flexWrap="nowrap">
+                <MacaronStack width="40px" count={12} />
+              </HStack>
+            </Box>
+            <Heading as="h2" size="md" color="primaryHighContrastText">
+              Design System and Styled UI Components
+            </Heading>
           </VStack>
+
           <Divider />
+
+          {/* Lower page */}
           <HStack gap={4} width="100%" alignItems="flex-start">
+            {/* Sidebar */}
             <VStack
-              gap={8}
-              minWidth={{ tablet: "3xs", mobile: "fit" }}
-              maxWidth="md"
+              gap={4}
+              width="100%"
+              maxWidth={{ tablet: "3xs", mobile: "4xs" }}
               alignItems="flex-start"
+              as="nav"
             >
-              <NavLink to="/docs/intro">Introduction</NavLink>
-              <NavLink to="/docs/philosophy">Philosophy</NavLink>
+              <NavBarLink to="/docs/intro">Introduction</NavBarLink>
+              <NavBarLink to="/docs/philosophy">Philosophy</NavBarLink>
+
               <Divider />
 
-              <Heading as="h3" size="sm">
-                Layout
-              </Heading>
-              <NavLink to="/docs/box">Box</NavLink>
-              <NavLink to="/docs/stack">Stack</NavLink>
-              <NavLink to="/docs/card">Card</NavLink>
+              <NavBarHeading>Theme/Design System</NavBarHeading>
+
+              <NavBarLink to="/docs/color">Color</NavBarLink>
+              <NavBarLink to="/docs/darkmode">Dark mode</NavBarLink>
+              <NavBarLink to="/docs/spacing">Spacing</NavBarLink>
+              <NavBarLink to="/docs/border">Border and Radius</NavBarLink>
+              <NavBarLink to="/docs/breakpoints">Breakpoints</NavBarLink>
+              <NavBarLink to="/docs/shadows">Shadows</NavBarLink>
+
               <Divider />
-              <Heading as="h3" size="sm">
-                Components
-              </Heading>
-              <NavLink to="/docs/alert">Alert</NavLink>
-              <NavLink to="/docs/alert-dialog">Alert Dialog</NavLink>
-              <NavLink to="/docs/badge">Badge</NavLink>
-              <NavLink to="/docs/button">Button</NavLink>
-              <NavLink to="/docs/checkbox">Checkbox</NavLink>
-              <NavLink to="/docs/dialog">Dialog</NavLink>
-              <NavLink to="/docs/divider">Divider</NavLink>
-              <NavLink to="/docs/dropdown">Dropdown</NavLink>
-              <NavLink to="/docs/input">Input</NavLink>
-              <NavLink to="/docs/label">Label</NavLink>
-              <NavLink to="/docs/popover">Popover</NavLink>
-              <NavLink to="/docs/select">Select</NavLink>
-              <NavLink to="/docs/spinner">Spinner</NavLink>
-              <NavLink to="/docs/tooltip">Tooltip</NavLink>
+
+              <NavBarHeading>Layout</NavBarHeading>
+
+              <NavBarLink to="/docs/box">Box</NavBarLink>
+              <NavBarLink to="/docs/stack">Stack</NavBarLink>
+              <NavBarLink to="/docs/card">Card</NavBarLink>
+
               <Divider />
-              <Heading as="h3" size="sm">
-                Typography
-              </Heading>
-              <NavLink to="/docs/text">Text</NavLink>
-              <NavLink to="/docs/heading">Heading</NavLink>
+
+              <NavBarHeading>Components</NavBarHeading>
+
+              <NavBarLink to="/docs/alert">Alert</NavBarLink>
+              <NavBarLink to="/docs/alert-dialog">Alert Dialog</NavBarLink>
+              <NavBarLink to="/docs/badge">Badge</NavBarLink>
+              <NavBarLink to="/docs/button">Button</NavBarLink>
+              <NavBarLink to="/docs/checkbox">Checkbox</NavBarLink>
+              <NavBarLink to="/docs/dialog">Dialog</NavBarLink>
+              <NavBarLink to="/docs/divider">Divider</NavBarLink>
+              <NavBarLink to="/docs/dropdown">Dropdown</NavBarLink>
+              <NavBarLink to="/docs/input">Input</NavBarLink>
+              <NavBarLink to="/docs/label">Label</NavBarLink>
+              <NavBarLink to="/docs/popover">Popover</NavBarLink>
+              <NavBarLink to="/docs/select">Select</NavBarLink>
+              <NavBarLink to="/docs/spinner">Spinner</NavBarLink>
+              <NavBarLink to="/docs/tooltip">Tooltip</NavBarLink>
+
+              <Divider />
+
+              <NavBarHeading>Typography</NavBarHeading>
+
+              <NavBarLink to="/docs/text">Text</NavBarLink>
+              <NavBarLink to="/docs/heading">Heading</NavBarLink>
             </VStack>
-            <VStack gap={8} width="100%" alignItems="flex-start" marginX="auto">
+
+            {/* Main Content Panel */}
+            <Box paddingBottom={8} maxWidth="prose" width="100%" as="article">
               <Outlet />
-            </VStack>
+            </Box>
           </HStack>
         </VStack>
       </TooltipProvider>

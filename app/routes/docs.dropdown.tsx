9b237e43f3ad054701a85c34dcf7bb882a2800f9
Jory Phillips <jory.l.phillips@gmail.com>
2024-08-16 16:14:08 -0700

    chore: update dropdown docs

--- app/routes/docs.dropdown.tsx
+++ app/routes/docs.dropdown.tsx
@@ -6,12 +6,19 @@ import {
   Button,
   DropdownMenu,
   DropdownMenuTrigger,
+  Text,
 } from "~/components/ui";
 
 export default function DropdownRoute() {
   return (
     <VStack gap={8} width="100%" alignItems="flex-start">
       <Heading>Dropdown</Heading>
+      <Text as="p">
+        The dropdown component is used to show a list of actions or options when
+        a user clicks on a trigger element. It is distinguished from a Select
+        component in that it is not intended to store or display a selected
+        value.
+      </Text>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="outline">Open</Button>

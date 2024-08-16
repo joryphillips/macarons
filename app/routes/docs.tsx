4ac00b64726dfe0513e6a8772682f15ed7ced399
Jory Phillips <jory.l.phillips@gmail.com>
2024-08-16 16:17:01 -0700

    chore: doc layout for mobile

--- app/routes/docs.tsx
+++ app/routes/docs.tsx
@@ -1,10 +1,22 @@
 import { Outlet, useRouteLoaderData } from "@remix-run/react";
 import type { UserTheme } from "~/components/ThemeProvider/ThemeProvider";
 import { ThemeProvider } from "~/components/ThemeProvider/ThemeProvider";
-import { Box, Divider, HStack, Heading, VStack, Text } from "~/components/ui";
+import {
+  Box,
+  Divider,
+  HStack,
+  Heading,
+  VStack,
+  IconButton,
+  StackProps,
+} from "~/components/ui";
 import { TooltipProvider } from "~/components/ui/Tooltip/Tooltip";
-import { NavBarLink } from "~/components/ui/NavBarLink/NavBarLink";
-import { MacaronStack } from "~/components/logo/MacaronStack";
+import { NavBarLink } from "~/components/NavBarLink/NavBarLink";
+import { Titlemark } from "~/components/logo/Titlemark";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import { ThemeToggle } from "~/components/ThemeToggle/ThemeToggle";
+import { vars } from "~/theme/theme.css";
 
 function NavBarHeading({ children }: { children: React.ReactNode }) {
   return (
@@ -20,105 +32,77 @@ export default function UI() {
   const rootMatchesData = useRouteLoaderData("root");
   const theme = rootMatchesData?.theme as UserTheme;
 
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => setShowSidebar(!showSidebar);
+  const hideSidebar = () => setShowSidebar(false);
+
   return (
     <ThemeProvider specifiedTheme={theme}>
       <TooltipProvider>
+        {/* Top bar */}
+        <HStack
+          gap={1}
+          padding={1}
+          position="fixed"
+          top={0}
+          width="100%"
+          justifyContent="flex-end"
+          alignItems="center"
+          backgroundColor="appBackground"
+          borderWidth="standard"
+          borderColor="subtleBorder"
+          borderBottomStyle="solid"
+          zIndex={1}
+        >
+          <ThemeToggle />
+          <Box display={{ mobile: "flex", tablet: "none" }}>
+            <IconButton variant="ghost" onClick={toggleSidebar}>
+              {showSidebar ? (
+                <X size="1.5rem" color={vars.colors.primary.lowContrastText} />
+              ) : (
+                <Menu
+                  size="1.5rem"
+                  color={vars.colors.primary.lowContrastText}
+                />
+              )}
+            </IconButton>
+          </Box>
+        </HStack>
+
+        {/* Main Content */}
         <VStack
           as="main"
           height="100vh"
           maxWidth="xl"
           marginX="auto"
           spacing={8}
-          paddingTop={4}
-          paddingX={4}
+          paddingTop={12}
+          paddingX={showSidebar ? 0 : 4}
           paddingBottom={8}
           alignItems="center"
+          overflowY={showSidebar ? "hidden" : "scroll"}
         >
           {/* Page header */}
-          <VStack width="100%" gap={4} alignItems="center">
-            <Box marginY={2} width="100%">
-              <Heading
-                color="primaryHighContrastText"
-                align="center"
-                size="4xl"
-                style={{
-                  fontFamily: "Georgia, serif",
-                  fontWeight: "bold",
-                  fontStyle: "italic",
-                  transform: "scaleX(0.86)",
-                }}
-              >
-                macarons
-              </Heading>
-              <HStack gap={0} justifyContent="center" flexWrap="nowrap">
-                <MacaronStack width="40px" count={12} />
-              </HStack>
-            </Box>
-            <Heading as="h2" size="md" color="primaryHighContrastText">
-              Design System and Styled UI Components
-            </Heading>
-          </VStack>
+          <Titlemark />
 
           <Divider />
 
           {/* Lower page */}
-          <HStack gap={4} width="100%" alignItems="flex-start">
+          <HStack
+            width="100%"
+            justifyContent={{ mobile: "flex-start", desktop: "center" }}
+            alignItems="flex-start"
+            gap={8}
+          >
             {/* Sidebar */}
-            <VStack
-              gap={4}
-              width="100%"
-              maxWidth={{ tablet: "3xs", mobile: "4xs" }}
-              alignItems="flex-start"
-              as="nav"
-            >
-              <NavBarLink to="/docs/intro">Introduction</NavBarLink>
-              <NavBarLink to="/docs/philosophy">Philosophy</NavBarLink>
-
-              <Divider />
-
-              <NavBarHeading>Theme/Design System</NavBarHeading>
-
-              <NavBarLink to="/docs/color">Color</NavBarLink>
-              <NavBarLink to="/docs/darkmode">Dark mode</NavBarLink>
-              <NavBarLink to="/docs/spacing">Spacing</NavBarLink>
-              <NavBarLink to="/docs/border">Border and Radius</NavBarLink>
-              <NavBarLink to="/docs/breakpoints">Breakpoints</NavBarLink>
-              <NavBarLink to="/docs/shadows">Shadows</NavBarLink>
-
-              <Divider />
-
-              <NavBarHeading>Layout</NavBarHeading>
-
-              <NavBarLink to="/docs/box">Box</NavBarLink>
-              <NavBarLink to="/docs/stack">Stack</NavBarLink>
-              <NavBarLink to="/docs/card">Card</NavBarLink>
-
-              <Divider />
-
-              <NavBarHeading>Components</NavBarHeading>
-
-              <NavBarLink to="/docs/alert">Alert</NavBarLink>
-              <NavBarLink to="/docs/alert-dialog">Alert Dialog</NavBarLink>
-              <NavBarLink to="/docs/badge">Badge</NavBarLink>
-              <NavBarLink to="/docs/button">Button</NavBarLink>
-              <NavBarLink to="/docs/checkbox">Checkbox</NavBarLink>
-              <NavBarLink to="/docs/dialog">Dialog</NavBarLink>
-              <NavBarLink to="/docs/divider">Divider</NavBarLink>
-              <NavBarLink to="/docs/dropdown">Dropdown</NavBarLink>
-              <NavBarLink to="/docs/input">Input</NavBarLink>
-              <NavBarLink to="/docs/label">Label</NavBarLink>
-              <NavBarLink to="/docs/popover">Popover</NavBarLink>
-              <NavBarLink to="/docs/select">Select</NavBarLink>
-              <NavBarLink to="/docs/spinner">Spinner</NavBarLink>
-              <NavBarLink to="/docs/tooltip">Tooltip</NavBarLink>
-
-              <Divider />
-
-              <NavBarHeading>Typography</NavBarHeading>
-
-              <NavBarLink to="/docs/text">Text</NavBarLink>
-              <NavBarLink to="/docs/heading">Heading</NavBarLink>
-            </VStack>
+            <SideNav
+              display={{
+                mobile: showSidebar ? "flex" : "none",
+                tablet: "flex",
+              }}
+              hideSidebar={hideSidebar}
+            />
 
             {/* Main Content Panel */}
             <Box paddingBottom={8} maxWidth="prose" width="100%" as="article">
@@ -128,5 +112,136 @@ export default function UI() {
         </VStack>
       </TooltipProvider>
     </ThemeProvider>
+  );
+}
+
+function SideNav({
+  hideSidebar,
+  ...props
+}: StackProps & { hideSidebar: () => void }) {
+  return (
+    <VStack
+      position={{ mobile: "fixed", tablet: "relative" }}
+      maxWidth={{ tablet: "3xs", mobile: "100%" }}
+      top={{ mobile: 12, tablet: "auto" }}
+      gap={{ mobile: 0, tablet: 4 }}
+      paddingTop={{ mobile: 4, tablet: 0 }}
+      paddingBottom={12}
+      width="100%"
+      alignItems="flex-start"
+      backgroundColor="appBackground"
+      height={{ mobile: "100vh", tablet: "auto" }}
+      overflowY={{ mobile: "scroll", tablet: "visible" }}
+      as="nav"
+      {...props}
+    >
+      <VStack
+        width="100%"
+        alignItems="flex-start"
+        paddingX={{ mobile: 2, tablet: 0 }}
+      >
+        <NavBarLink onClick={hideSidebar} to="/docs/intro">
+          Introduction
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/philosophy">
+          Philosophy
+        </NavBarLink>
+
+        <Divider />
+
+        <NavBarHeading>Theme/Design System</NavBarHeading>
+
+        <NavBarLink onClick={hideSidebar} to="/docs/color">
+          Color
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/darkmode">
+          Dark mode
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/shadows">
+          Shadows
+        </NavBarLink>
+        <NavBarLink disabled onClick={hideSidebar} to="/docs/spacing">
+          Spacing
+        </NavBarLink>
+        <NavBarLink disabled onClick={hideSidebar} to="/docs/border">
+          Border and Radius
+        </NavBarLink>
+        <NavBarLink disabled onClick={hideSidebar} to="/docs/breakpoints">
+          Breakpoints
+        </NavBarLink>
+
+        <Divider />
+
+        <NavBarHeading>Layout</NavBarHeading>
+
+        <NavBarLink onClick={hideSidebar} to="/docs/box">
+          Box
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/stack">
+          Stack
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/card">
+          Card
+        </NavBarLink>
+
+        <Divider />
+
+        <NavBarHeading>Components</NavBarHeading>
+
+        <NavBarLink onClick={hideSidebar} to="/docs/alert">
+          Alert
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/alert-dialog">
+          Alert Dialog
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/badge">
+          Badge
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/button">
+          Button
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/checkbox">
+          Checkbox
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/dialog">
+          Dialog
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/divider">
+          Divider
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/dropdown">
+          Dropdown
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/input">
+          Input
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/label">
+          Label
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/popover">
+          Popover
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/select">
+          Select
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/spinner">
+          Spinner
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/tooltip">
+          Tooltip
+        </NavBarLink>
+
+        <Divider />
+
+        <NavBarHeading>Typography</NavBarHeading>
+
+        <NavBarLink onClick={hideSidebar} to="/docs/text">
+          Text
+        </NavBarLink>
+        <NavBarLink onClick={hideSidebar} to="/docs/heading">
+          Heading
+        </NavBarLink>
+      </VStack>
+    </VStack>
   );
 }

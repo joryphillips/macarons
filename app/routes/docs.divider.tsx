b78e992f7d724a960c95964e5d4cb9ee8c1b56f3
Jory Phillips <jory.l.phillips@gmail.com>
2024-08-16 16:13:54 -0700

    chore: update divider docs

--- app/routes/docs.divider.tsx
+++ app/routes/docs.divider.tsx
@@ -4,6 +4,10 @@ export default function DividerRoute() {
   return (
     <VStack gap={8} width="100%" alignItems="flex-start">
       <Heading>Divider</Heading>
+      <Text as="p">
+        The Divider component will automatically fill the width of its
+        container.{" "}
+      </Text>
       <Text as="p">Content above the divider</Text>
       <Divider />
       <Text as="p">Content below the divider</Text>

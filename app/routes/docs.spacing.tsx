ee52ee79e63cf34929d6295ddd856dde11684185
Jory Phillips <jory.l.phillips@gmail.com>
2024-08-20 16:22:49 -0700

    feat: add spacing docs

---
+++ app/routes/docs.spacing.tsx
@@ -0,0 +1,56 @@
+import { Box, Heading, HStack, Text, VStack } from "~/components/ui";
+import { vars } from "~/theme/theme.css";
+import { spacing } from "~/theme/spacing";
+
+function DemoBox() {
+  return (
+    <Box
+      style={{
+        height: "100px",
+        width: "100px",
+      }}
+      backgroundColor="elementBackground"
+      borderColor="elementBorder"
+      borderStyle="solid"
+      borderRadius="sm"
+    />
+  );
+}
+
+function SpacingDemoRow({
+  spacing: space,
+}: {
+  spacing: keyof typeof vars.spacing;
+}) {
+  return (
+    <VStack gap={1} alignItems="flex-start">
+      <Text>
+        Spacing: {space} ({spacing[space]})
+      </Text>
+      <HStack gap={space}>
+        <DemoBox />
+        <DemoBox />
+      </HStack>
+    </VStack>
+  );
+}
+
+export default function SpacingRoute() {
+  return (
+    <VStack marginBottom={8} width="100%" alignItems="flex-start">
+      <Heading>Spacing</Heading>
+      <Text as="p">
+        Like other tokens, spacing is defined in an independent file and
+        imported into common variables in the theme, as well as Sprinkles
+        variables that are then made available everywhere. The values follow
+        patterns familiar to Chakra UI and Tailwind users and are easily
+        customizable.
+      </Text>
+      <VStack width="100%" alignItems="flex-start">
+        {Object.keys(vars.spacing).map((spacing) => (
+          <SpacingDemoRow spacing={spacing as keyof typeof vars.spacing} />
+        ))}
+      </VStack>
+    </VStack>
+  );
+}

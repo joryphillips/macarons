0eb7f9f3775de2c9a9ba93b281cbc90e8859b2a4
Jory Phillips <jory.l.phillips@gmail.com>
2024-08-26 17:26:17 -0700

    chore: normalize tooltip styles to theme variables

--- app/components/ui/Tooltip/Tooltip.css.ts
+++ app/components/ui/Tooltip/Tooltip.css.ts
@@ -2,15 +2,12 @@ import { style } from "@vanilla-extract/css";
 import { vars } from "~/theme/theme.css";
 
 export const tooltipContentStyles = style({
+  zIndex: 9999,
   borderRadius: vars.border.radius.sm,
-  borderWidth: "1px",
-  padding: "0.75rem 0.75rem",
-  fontSize: "0.875rem",
+  padding: vars.spacing[3],
   color: vars.colors.standard.appBackground,
   backgroundColor: vars.colors.primary.solidBackground,
   opacity: "0.93",
   boxShadow: vars.boxShadow.md,
   overflow: "hidden",
-  fontWeight: 400,
-  lineHeight: "1.25rem",
 });

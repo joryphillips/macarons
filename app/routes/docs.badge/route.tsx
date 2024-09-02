5f0bfbce02bb4de1919d3d8081f863ed96c85a3f
Jory Phillips <jory.l.phillips@gmail.com>
2024-09-02 10:37:36 -0700

    feat: Badge formatted code examples

--- app/routes/docs.badge/route.tsx
+++ app/routes/docs.badge/route.tsx
@@ -1,21 +1,45 @@
+import { SyntaxHighlighter } from "~/components/SyntaxHighlighter/SyntaxHighlighter";
 import { VStack, Heading, Text, Badge } from "~/components/ui";
+
+const badgeStandard = `<Badge variant="standard">Standard</Badge>`;
+const badgeInfo = `<Badge variant="info">Info</Badge>`;
+const badgeSuccess = `<Badge variant="success">Success</Badge>`;
+const badgeWarning = `<Badge variant="warning">Warning</Badge>`;
+const badgeError = `<Badge variant="error">Error</Badge>`;
 
 export default function BadgeRoute() {
   return (
     <VStack gap={8} width="100%" alignItems="flex-start">
-      <Heading>Badge</Heading>
-      <Text as="p">
-        The badge component is used to display a small amount of information
-        like a count or status.
-      </Text>
+      <VStack gap={4} alignItems="flex-start" width="100%">
+        <Heading>Badge</Heading>
+        <Text as="p">
+          The badge component is used to display a small amount of information
+          like a count or status.
+        </Text>
+      </VStack>
       <Badge>Badge</Badge>
       <Heading size="md">Variants</Heading>
-      <VStack gap={2} alignItems="flex-start">
-        <Badge variant="standard">Standard</Badge>
-        <Badge variant="info">Info</Badge>
-        <Badge variant="success">Success</Badge>
-        <Badge variant="warning">Warning</Badge>
-        <Badge variant="error">Error</Badge>
+      <VStack gap={6} alignItems="flex-start">
+        <VStack gap={2} alignItems="flex-start">
+          <Badge variant="standard">Standard</Badge>
+          <SyntaxHighlighter copy={false}>{badgeStandard}</SyntaxHighlighter>
+        </VStack>
+        <VStack gap={2} alignItems="flex-start">
+          <Badge variant="info">Info</Badge>
+          <SyntaxHighlighter copy={false}>{badgeInfo}</SyntaxHighlighter>
+        </VStack>
+        <VStack gap={2} alignItems="flex-start">
+          <Badge variant="success">Success</Badge>
+          <SyntaxHighlighter copy={false}>{badgeSuccess}</SyntaxHighlighter>
+        </VStack>
+        <VStack gap={2} alignItems="flex-start">
+          <Badge variant="warning">Warning</Badge>
+          <SyntaxHighlighter copy={false}>{badgeWarning}</SyntaxHighlighter>
+        </VStack>
+        <VStack gap={2} alignItems="flex-start">
+          <Badge variant="error">Error</Badge>
+          <SyntaxHighlighter copy={false}>{badgeError}</SyntaxHighlighter>
+        </VStack>
       </VStack>
     </VStack>
   );

be1fc82d54af218f8155b0bb4e21bde4918347b5
Jory Phillips <jory.l.phillips@gmail.com>
2024-09-03 15:22:32 -0700

    fix: stack docs spacing

--- app/routes/docs.stack/route.tsx
+++ app/routes/docs.stack/route.tsx
@@ -13,46 +13,59 @@ import CustomExampleString from "./CustomExample?raw";
 
 export default function StackRoute() {
   return (
-    <VStack gap={4} width="100%" alignItems="flex-start">
-      <Heading>Stack</Heading>
-      <Text as="p">
-        The stack component is used to layout elements in a vertical or
-        horizontal stack.
-      </Text>
+    <VStack gap={8} width="100%" alignItems="flex-start">
+      <VStack gap={4} width="100%" alignItems="flex-start">
+        <Heading>Stack</Heading>
+        <Text as="p">
+          The stack component is used to layout elements in a vertical or
+          horizontal stack.
+        </Text>
+      </VStack>
 
-      <Heading size="md">Responsive Stack</Heading>
-      <Text as="p">
-        The base stack component can be used in combination with a media query
-        to dynamically change the direction of the stack based on the width of
-        the screen.
-      </Text>
-      <ResponsiveExample />
-      <SyntaxHighlighter>{ResponsiveExampleString}</SyntaxHighlighter>
+      <VStack gap={4} width="100%" alignItems="flex-start">
+        <Heading size="md">Responsive Stack</Heading>
+        <Text as="p">
+          The base stack component can be used in combination with a media query
+          to dynamically change the direction of the stack based on the width of
+          the screen.
+        </Text>
+        <ResponsiveExample />
+        <SyntaxHighlighter>{ResponsiveExampleString}</SyntaxHighlighter>
+      </VStack>
 
-      <Heading size="md">Vertical Stack</Heading>
-      <Text>The VStack implements a flex column.</Text>
-      <VStackExample />
-      <SyntaxHighlighter>{VStackExampleString}</SyntaxHighlighter>
-      <Heading size="md">Horizontal Stack</Heading>
-      <Text>The HStack implements a flex row.</Text>
-      <HStackExample />
-      <SyntaxHighlighter>{HStackExampleString}</SyntaxHighlighter>
+      <VStack gap={4} width="100%" alignItems="flex-start">
+        <Heading size="md">Vertical Stack</Heading>
+        <Text>The VStack implements a flex column.</Text>
+        <VStackExample />
+        <SyntaxHighlighter>{VStackExampleString}</SyntaxHighlighter>
+      </VStack>
 
-      <Heading size="md">Gap</Heading>
-      <Text>
-        The gap prop can be used to add space between children of the stack. A
-        prop called "spacing" is also available as an alias for gap.
-      </Text>
-      <GapExample />
-      <SyntaxHighlighter>{GapExampleString}</SyntaxHighlighter>
+      <VStack gap={4} width="100%" alignItems="flex-start">
+        <Heading size="md">Horizontal Stack</Heading>
+        <Text>The HStack implements a flex row.</Text>
+        <HStackExample />
+        <SyntaxHighlighter>{HStackExampleString}</SyntaxHighlighter>
+      </VStack>
 
-      <Heading size="md">Customization</Heading>
-      <Text>
-        All props available to Box can be used with a Stack, HStack, or VStack
-        component.
-      </Text>
-      <CustomExample />
-      <SyntaxHighlighter>{CustomExampleString}</SyntaxHighlighter>
+      <VStack gap={4} width="100%" alignItems="flex-start">
+        <Heading size="md">Gap</Heading>
+        <Text>
+          The gap prop can be used to add space between children of the stack. A
+          prop called "spacing" is also available as an alias for gap.
+        </Text>
+        <GapExample />
+        <SyntaxHighlighter>{GapExampleString}</SyntaxHighlighter>
+      </VStack>
+
+      <VStack gap={4} width="100%" alignItems="flex-start">
+        <Heading size="md">Customization</Heading>
+        <Text>
+          All props available to Box can be used with a Stack, HStack, or VStack
+          component.
+        </Text>
+        <CustomExample />
+        <SyntaxHighlighter>{CustomExampleString}</SyntaxHighlighter>
+      </VStack>
     </VStack>
   );
 }

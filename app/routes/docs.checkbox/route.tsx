1cae0c31013f63302cd063d7d443025459d8390b
Jory Phillips <jory.l.phillips@gmail.com>
2024-09-07 09:10:02 -0700

    feat: checkbox code examples

--- app/routes/docs.checkbox/route.tsx
+++ app/routes/docs.checkbox/route.tsx
@@ -1,25 +1,50 @@
+import { SyntaxHighlighter } from "~/components/SyntaxHighlighter/SyntaxHighlighter";
 import { VStack, Heading, Checkbox, HStack } from "~/components/ui";
+
+const defaultCheckboxExample = `<HStack gap={2}>
+  <Checkbox id="default" />
+  <label htmlFor="default">Default</label>
+</HStack>`;
+
+const checkedCheckboxExample = `<Checkbox id="checked" defaultChecked />`;
+const disabledCheckboxExample = `<Checkbox id="disabled" disabled />`;
+const checkedDisabledCheckboxExample = `<Checkbox id="checkedDisabled" defaultChecked disabled />`;
 
 export default function CheckboxRoute() {
   return (
     <VStack gap={8} width="100%" alignItems="flex-start">
       <Heading>Checkbox</Heading>
-      <HStack gap={2}>
-        <Checkbox id="default" />
-        <label htmlFor="default">Default</label>
-      </HStack>
-      <HStack gap={2}>
-        <Checkbox id="checked" defaultChecked />
-        <label htmlFor="checked">Checked</label>
-      </HStack>
-      <HStack gap={2}>
-        <Checkbox id="disabled" disabled />
-        <label htmlFor="disabled">Disabled</label>
-      </HStack>
-      <HStack gap={2}>
-        <Checkbox id="checkedDisabled" defaultChecked disabled />
-        <label htmlFor="checkedDisabled">Checked and Disabled</label>
-      </HStack>
+      <VStack alignItems="flex-start" width="100%">
+        <HStack gap={2}>
+          <Checkbox id="default" />
+          <label htmlFor="default">Default</label>
+        </HStack>
+        <SyntaxHighlighter>{defaultCheckboxExample}</SyntaxHighlighter>
+      </VStack>
+
+      <VStack alignItems="flex-start" width="100%">
+        <HStack gap={2}>
+          <Checkbox id="checked" defaultChecked />
+          <label htmlFor="checked">Checked</label>
+        </HStack>
+        <SyntaxHighlighter>{checkedCheckboxExample}</SyntaxHighlighter>
+      </VStack>
+
+      <VStack alignItems="flex-start" width="100%">
+        <HStack gap={2}>
+          <Checkbox id="disabled" disabled />
+          <label htmlFor="disabled">Disabled</label>
+        </HStack>
+        <SyntaxHighlighter>{disabledCheckboxExample}</SyntaxHighlighter>
+      </VStack>
+
+      <VStack alignItems="flex-start" width="100%">
+        <HStack gap={2}>
+          <Checkbox id="checkedDisabled" defaultChecked disabled />
+          <label htmlFor="checkedDisabled">Checked and Disabled</label>
+        </HStack>
+        <SyntaxHighlighter>{checkedDisabledCheckboxExample}</SyntaxHighlighter>
+      </VStack>
     </VStack>
   );
 }

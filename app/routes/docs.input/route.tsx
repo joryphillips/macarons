9bda97ffa41d2392ace297d1fa449c30b54be585
Jory Phillips <jory.l.phillips@gmail.com>
2024-09-10 15:16:19 -0700

    feat: Input doc code examples

--- app/routes/docs.input/route.tsx
+++ app/routes/docs.input/route.tsx
@@ -1,118 +1,124 @@
-import { useState } from "react";
-import {
-  VStack,
-  Heading,
-  Input,
-  InputWithInfo,
-  InputWithLabel,
-  Text,
-} from "~/components/ui";
+import { VStack, Heading, Text } from "~/components/ui";
+import { SyntaxHighlighter } from "~/components/SyntaxHighlighter/SyntaxHighlighter";
+import { Minimal } from "./Minimal";
+import minimalString from "./Minimal?raw";
+import { Clearable } from "./Clearable";
+import clearableString from "./Clearable?raw";
+import { Wide } from "./Wide";
+import wideString from "./Wide?raw";
+import { Invalid } from "./Invalid";
+import invalidString from "./Invalid?raw";
+import { WithLabel } from "./WithLabel";
+import withLabelString from "./WithLabel?raw";
+import { WithInfo } from "./WithInfo";
+import withInfoString from "./WithInfo?raw";
+import { WithError } from "./WithError";
+import withErrorString from "./WithError?raw";
+import { Small } from "./Small";
+import withSmallString from "./Small?raw";
 
 export default function CheckboxRoute() {
-  const [clearableInputValue, setClearableInputValue] = useState("clearable");
-  const [wideInputValue, setWideInputValue] = useState("wide");
-
-  const [clearableSmallInputValue, setClearableSmallInputValue] =
-    useState("small clearable");
-
   return (
-    <VStack gap={8} width="100%" alignItems="flex-start" as="article">
-      <Heading>Input</Heading>
-      <Text as="p">
-        The Input component is used to collect text input from users, often in a
-        form. It can be used in a variety of sizes and states.
-      </Text>
-      <VStack alignItems="flex-start" gap={3} as="section">
-        <Heading as="h3" size="md">
+    <VStack gap={8} width="100%" alignItems="flex-start">
+      <VStack gap={4} alignItems="flex-start" width="100%" as="section">
+        <Heading as="h1" size="xl">
+          Input
+        </Heading>
+        <Text as="p">
+          The Input component is used to collect text input from users, often in
+          a form. It can be used in a variety of sizes and states.
+        </Text>
+      </VStack>
+
+      <VStack width="100%" alignItems="flex-start" gap={4} as="section">
+        <Heading as="h2" size="md">
           Minimal
         </Heading>
-        <Input name="minimal" placeholder="placeholder" />
+        {/* Add VStack wrapper without width to demonstrate input width default */}
+        <VStack>
+          <Minimal />
+        </VStack>
+        <SyntaxHighlighter trimToReactReturnValue>
+          {minimalString}
+        </SyntaxHighlighter>
       </VStack>
-      <VStack alignItems="flex-start" gap={3} as="section">
-        <Heading as="h3" size="md">
-          Clearable
-        </Heading>
-        <InputWithLabel
-          label="Clearable"
-          placeholder="placeholder"
-          value={clearableInputValue}
-          onClear={() => setClearableInputValue("")}
-          onChange={(e) => setClearableInputValue(e.target.value)}
-        />
-      </VStack>
-      <VStack alignItems="flex-start" gap={3} as="section" width="100%">
-        <Heading as="h3" size="md">
+
+      <VStack alignItems="flex-start" gap={4} as="section" width="100%">
+        <Heading as="h2" size="md">
           Wide
         </Heading>
-        <InputWithLabel
-          width="full"
-          label="Wide"
-          placeholder="placeholder"
-          value={wideInputValue}
-          onClear={() => setWideInputValue("")}
-          onChange={(e) => setWideInputValue(e.target.value)}
-        />
+        <Text as="p">
+          Input will expand to the width of its parent container.
+        </Text>
+        <Wide />
+        <SyntaxHighlighter trimToReactReturnValue>
+          {wideString}
+        </SyntaxHighlighter>
       </VStack>
-      <VStack alignItems="flex-start" gap={3} as="section">
-        <Heading as="h3" size="md">
+
+      <VStack alignItems="flex-start" gap={4} as="section">
+        <Heading as="h2" size="md">
           Invalid state
         </Heading>
-        <Input name="minimal" isInvalid placeholder="placeholder" />
+        <VStack>
+          <Invalid />
+        </VStack>
+        <SyntaxHighlighter trimToReactReturnValue>
+          {invalidString}
+        </SyntaxHighlighter>
       </VStack>
-      <VStack alignItems="flex-start" gap={3} as="section">
-        <Heading as="h3" size="md">
+
+      <VStack alignItems="flex-start" gap={4} as="section">
+        <Heading as="h2" size="md">
           With Label
         </Heading>
-        <InputWithLabel
-          id="with-label"
-          name="label"
-          placeholder="placeholder"
-          label="Label"
-        />
+        <VStack>
+          <WithLabel />
+        </VStack>
+        <SyntaxHighlighter trimToReactReturnValue>
+          {withLabelString}
+        </SyntaxHighlighter>
       </VStack>
-      <VStack alignItems="flex-start" gap={3} as="section">
-        <Heading as="h3" size="md">
+
+      <VStack alignItems="flex-start" gap={4} as="section">
+        <Heading as="h2" size="md">
+          Clearable
+        </Heading>
+        <VStack>
+          <Clearable />
+        </VStack>
+        <SyntaxHighlighter>{clearableString}</SyntaxHighlighter>
+      </VStack>
+
+      <VStack alignItems="flex-start" gap={4} as="section">
+        <Heading as="h2" size="md">
           With Info
         </Heading>
-        <InputWithInfo
-          name="info"
-          placeholder="placeholder"
-          label="Label"
-          infoText="Descriptive text"
-        />
+        <VStack>
+          <WithInfo />
+        </VStack>
+        <SyntaxHighlighter trimToReactReturnValue minWidth="2xs">
+          {withInfoString}
+        </SyntaxHighlighter>
       </VStack>
-      <VStack alignItems="flex-start" gap={3} as="section">
-        <Heading as="h3" size="md">
+
+      <VStack alignItems="flex-start" gap={4} as="section">
+        <Heading as="h2" size="md">
           Info with Error State
         </Heading>
-        <InputWithInfo
-          name="error"
-          placeholder="placeholder"
-          defaultValue="Invalid input value"
-          label="Label"
-          errorText="Descriptive error text"
-        />
+        <WithError />
+        <SyntaxHighlighter trimToReactReturnValue minWidth="2xs">
+          {withErrorString}
+        </SyntaxHighlighter>
       </VStack>
-      <VStack alignItems="flex-start" gap={3} as="section">
-        <Heading as="h3" size="md">
+      <VStack alignItems="flex-start" gap={4} as="section">
+        <Heading as="h2" size="md">
           Small
         </Heading>
-        <Input name="small" size="sm" placeholder="placeholder" />{" "}
-        <InputWithLabel
-          name="small w label"
-          size="sm"
-          placeholder="placeholder"
-          label="small with label"
-        />
-        <InputWithLabel
-          name="small clearable w label"
-          size="sm"
-          placeholder="placeholder"
-          label="small with label"
-          value={clearableSmallInputValue}
-          onClear={() => setClearableSmallInputValue("")}
-          onChange={(e) => setClearableSmallInputValue(e.target.value)}
-        />
+        <Small />
+        <SyntaxHighlighter trimToReactReturnValue>
+          {withSmallString}
+        </SyntaxHighlighter>
       </VStack>
     </VStack>
   );
